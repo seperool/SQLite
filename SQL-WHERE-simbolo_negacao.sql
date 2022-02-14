92db0fc3f60900f5035bd802597339b71b7405b4
Sergio Pedro <seperool@gmail.com>
2022-02-14 08:05:56 -0300

    filto - filtragem de dados apartir da negação de um determinado valor

---
+++ SQL-WHERE-simbolo_negacao.sql
@@ -0,0 +1,6 @@
+/*WHERE*/
+/*filtragem de registro de acordo com criterios
+feito com a instruçao WHERE*/
+SELECT * FROM station_data
+WHERE year != 2010; /*tambem e possivel usar "<>" ao inves de "!=" para negaçao*/
+/*obtem tudo, exeto year de 2010*/
