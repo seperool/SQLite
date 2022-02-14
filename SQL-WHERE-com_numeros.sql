3d392267bab94423c8b64c3e6f56ac5c7d257a00
Sergio Pedro <seperool@gmail.com>
2022-02-14 08:05:01 -0300

    filtro - filtragem de dados apartir de um numero

---
+++ SQL-WHERE-com_numeros.sql
@@ -0,0 +1,7 @@
+/*WHERE*/
+/*filtragem de registro de acordo com criterios
+feito com a instruçao WHERE*/
+SELECT * FROM station_data
+WHERE year = 2010; /*ponto e virgula apenas no final da instruçao*/
+
+/*obs.: as instruçoes, como tabelas e colunas podem estar me minusculo ou maiusculo, nao faz diferença*/
