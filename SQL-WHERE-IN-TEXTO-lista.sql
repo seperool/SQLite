d88903c4768ebe0bfdbf5a42ef9c5e99e4482a9a
Sergio Pedro <seperool@gmail.com>
2022-02-15 04:37:16 -0300

    WHERE - IN - TEXTO - filtro usando lista de textos

---
+++ SQL-WHERE-IN-TEXTO-lista.sql
@@ -0,0 +1,6 @@
+/*WHERE*/
+/*filtragem de registro de acordo com criterios
+feito com a instruçao WHERE*/
+SELECT * FROM station_data
+WHERE report_code IN ('513A63','1F8A7B','EF616A'); /*texto entre ''*/
+/*WHERE usando lista de textos*/
