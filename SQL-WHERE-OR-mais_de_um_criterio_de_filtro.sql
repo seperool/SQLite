69a749a84872cdcfce56cce0d859480433cfbedc
Sergio Pedro <seperool@gmail.com>
2022-02-15 04:34:26 -0300

    WHERE - OR - uso do OR para mais de um criterio a ser atendido ao mesmo tempo

---
+++ SQL-WHERE-OR-mais_de_um_criterio_de_filtro.sql
@@ -0,0 +1,9 @@
+/*WHERE*/
+/*filtragem de registro de acordo com criterios
+feito com a instruçao WHERE*/
+SELECT * FROM station_data
+WHERE month = 3
+OR month = 6
+OR month = 9
+OR month = 12;
+/*uso do OR para mais de um criterio a ser atendido ao mesmo tempo.*/
