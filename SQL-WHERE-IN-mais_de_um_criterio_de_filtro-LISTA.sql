d17d835f196dcfbd89d46ce63c48d77dcc955fed
Sergio Pedro <seperool@gmail.com>
2022-02-15 04:38:35 -0300

    WHERE - IN - filtro utilizando lista

---
+++ SQL-WHERE-IN-mais_de_um_criterio_de_filtro-LISTA.sql
@@ -0,0 +1,7 @@
+/*WHERE*/
+/*filtragem de registro de acordo com criterios
+feito com a instruçao WHERE*/
+SELECT * FROM station_data
+WHERE month IN (3,6,9,12);
+/*fornece uma lista valida de valores*/
+/*outra forma de escrever diversos criterios, substitui o uso de OR*/
