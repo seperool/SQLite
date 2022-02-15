ab5ee9642bc0ad4685d29d63749b1ad3d0d9ff03
Sergio Pedro <seperool@gmail.com>
2022-02-15 04:36:15 -0300

    WHERE - TEXTO - filtro usando texto

---
+++ SQL-WHERE-TEXTO.sql
@@ -0,0 +1,6 @@
+/*WHERE*/
+/*filtragem de registro de acordo com criterios
+feito com a instruçao WHERE*/
+SELECT * FROM station_data
+WHERE report_code = '513A63'; /*texto entre ''*/
+/*WHERE usando texto*/
