0a394770918d0214ae7b61ce4ab49f7291bcd980
Sergio Pedro <seperool@gmail.com>
2022-02-15 04:40:58 -0300

    WHERE - LIKE - filtro atraves de caracteres curingas % e _

---
+++ SQL-WHERE-LIKE-caracteres_curingas.sql
@@ -0,0 +1,7 @@
+/*WHERE*/
+/*filtragem de registro de acordo com criterios
+feito com a instruçao WHERE*/
+SELECT * FROM station_data
+WHERE report_code LIKE 'B_C%';
+/*uso de caracteres curingas com a expressao LIKE*/
+/*caracteres curingas: '%' qualquer numero de caracteres, '_' um unico caractere*/
