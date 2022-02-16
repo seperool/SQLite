aec78faa6853cf98aa141431d0861c92ddd3e0b7
Sergio Pedro <seperool@gmail.com>
2022-02-16 06:26:10 -0300

    WHERE -IS NULL OR - inseri NULL no filtro junto com outros criterios

---
+++ SQL-WHERE-IS_NULL-OR-inserir_null_no_filtro_e_outros_criterios.sql
@@ -0,0 +1,8 @@
+/*WHERE*/
+/*filtragem de registro de acordo com criterios
+feito com a instruçao WHERE*/
+SELECT * FROM station_data
+WHERE precipitation IS NULL OR precipitation <= 0.5; /*inserir valor nulo na filtragem de dados*/
+/*NULL nao pode ser determinado por '='*/
+/*o valor NULL e aquele que nao apresenta valor, 'em branco'*/
+/*NULL e diferente de 0, e nem valor de texto vazio*/
