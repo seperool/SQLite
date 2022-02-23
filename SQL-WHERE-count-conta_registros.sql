461eaf29d3c424cf0c3c2b4f7c6a68158cdff057
Sergio Pedro <seperool@gmail.com>
2022-02-23 03:40:41 -0300

    WHERE - count - conta os registros, onde o filtro WHERE é valido

---
+++ SQL-WHERE-count-conta_registros.sql
@@ -0,0 +1,7 @@
+/*GROUP BY*/
+/*agregaçao de dados*/
+
+/*contar todos os registros*/
+SELECT COUNT(*) AS record_count FROM station_data
+WHERE tornado == 1;
+/*funcao 'count()' conta os registros, onde tornado iqual a 1*/
