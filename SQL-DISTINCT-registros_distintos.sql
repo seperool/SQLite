afd35c8c2242d3a747159fa36dd65a96a961ef8a
Sergio Pedro <seperool@gmail.com>
2022-02-25 02:33:50 -0300

    DISTINCT - obtem registros distintos, sem duplicatas, sem valores repetidos.

---
+++ SQL-DISTINCT-registros_distintos.sql
@@ -0,0 +1,5 @@
+/*DISTINCT*/
+/*obtendo registros distintos, sem duplicatas, sem valores repetidos.*/
+SELECT DISTINCT station_number, year FROM station_data
+ORDER BY year;
+/*apenas valores distintos de station_number*/
