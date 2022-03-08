879010a6aae45e50a95300f7958faa276ae8d94b
Sergio Pedro <seperool@gmail.com>
2022-03-08 11:50:34 -0300

    LEFT JOIN - GROUP BY - uso de mescla com todos os registros, inclusive NULL, agrupados em torno de determinados registros, uso de funções de agregaçao para transforma NULL em zero

---
+++ Capitulo8-JOIN/SQL-LEFT_JOIN_varios-GROUP_BY-funcao_agregacao.sql
@@ -0,0 +1,18 @@
+/*ASSOCIANDO VARIAS TABELAS*/
+/*LEFT JOIN*/
+/*mescla diversas tabelas em uma unica consulta, atraves de colunas chaves entre as tabelas mescladas*/
+/*apresenta todos os registros, ate os nao associados entre tabelas, retorna NULL*/
+/*GROUP BY*/
+/*agrupando dados em torno de determinados registos*/
+SELECT
+CUSTOMER.CUSTOMER_ID,
+NAME AS CUSTOMER_NAME,
+COALESCE(SUM(ORDER_QTY * PRICE),0) AS TOTAL_REVENUE /*Uso de funçao de agregaçao, transforma NULL em 0*/
+FROM CUSTOMER /*tabela 1*/
+LEFT JOIN CUSTOMER_ORDER /*mesclar tabela 2 com a tabela 1*/
+ON CUSTOMER.CUSTOMER_ID = CUSTOMER_ORDER.CUSTOMER_ID /*associaçao de coluna chave1 da tabela 1 a coluna chave1 da tabela 2*/
+LEFT JOIN PRODUCT /*mesclar tabela 3 com a tabela 2*/
+ON PRODUCT.PRODUCT_ID = CUSTOMER_ORDER.PRODUCT_ID /*associaçao de coluna chave2 da tabela 3 a coluna chave2 da tabela 2*/
+GROUP BY 1,2; /*Agrupando dados em torno de customer_id e customer_name*/
+/*Alem de mesclar tabelas, uso de GROUP BY para usar funçoes de agregaçao e agregando dados em torno registros especificos*/
+/*Uso do LEFT JOIN para recuperar todos os registros do customer_id, retorna NULL*/
