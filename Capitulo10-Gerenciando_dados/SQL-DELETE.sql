ba3ba7a2178e289d18a6e80c883a6446e69bfbc0
Sergio Pedro <seperool@gmail.com>
2022-03-11 15:05:47 -0300

    DELETE - deleta todos os registros de uma tabela

---
+++ Capitulo10-Gerenciando_dados/SQL-DELETE.sql
@@ -0,0 +1,6 @@
+/*DELETE*/
+/*deleta todos os registro de uma terminada tabela*/
+DELETE FROM ATTENDEE;
+/*antes de deletar os registro, vale substituir a instruçao DELETE por SELECT (*),
+para averiguar se sao realmente aqueles registros a serem deletados*/
+/*no MySQL ao inves de DELETE e usado o TRUNCATE TABLE*/
